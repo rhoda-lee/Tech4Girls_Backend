31b55b45ef4075e6efc45764f82db819e0bdc50a
rhoda-lee <rhodalee.dev@gmail.com>
2024-10-16 09:17:51 +0000

    added install_packages.sh script

---
+++ Bash_Scripting/install_packages.sh
@@ -0,0 +1,15 @@
+#!/bin/
+
+#This is a script meant to update package list and install 3 packages, ie, fortune, cowsay and cmatrix
+
+#Command to update package list
+sudo apt update
+
+#Command to install fortune
+sudo apt install fortune
+
+#Command to install cowsay
+sudo apt install cowsay
+
+#Command to install cmatrix
+sudo apt install cmatrix

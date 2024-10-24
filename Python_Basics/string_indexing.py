f8c72a67c075c59e28be69d80db4326aa69bc5a3
rhoda-lee <rhodalee.dev@gmail.com>
2024-10-24 14:44:59 +0000

    Added string indexing file

---
+++ Python_Basics/string_indexing.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+sentence = 'Python is powerful and easy to learn.'
+print(len(sentence))
+
+first_character = sentence[0]
+print(first_character)
+
+last_character = sentence[-1]
+print(last_character)
+
+powerful = sentence[10:18]
+print(powerful)
